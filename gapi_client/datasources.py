f0f1b3208de39fe95eb5e71b21910db2f9f52b12
lyremelody <lyremelody@163.com>
2017-11-08 14:42:04 +0800

    api for delete datasource

--- gapi_client/datasources.py
+++ gapi_client/datasources.py
@@ -40,7 +40,19 @@ def create_datasource_prometheus(api_token, data_source_name, url, access='proxy
     return create_datasource(api_token=api_token, 
         data_source_name=data_source_name, 
         data_source_type=DataSourceType.PROMETHEUS.value, 
-        url=url, access=access, basic_auth=basic_auth):
+        url=url, access=access, basic_auth=basic_auth)
+
+
+def delete_datasource_by_name(api_token, datasource_name):
+    uri = 'http://{0}:{1}/api/datasources/name/{2}'.format(GRAFANA_INFO['host'], GRAFANA_INFO['port'], datasource_name)
+    r = requests.delete(uri, headers=get_auth_headers(api_token))
+    return (r.status_code, r.text)
+
+
+def delete_datasource_by_id(api_token, datasource_id):
+    uri = 'http://{0}:{1}/api/datasources/{2}'.format(GRAFANA_INFO['host'], GRAFANA_INFO['port'], datasource_id)
+    r = requests.delete(uri, headers=get_auth_headers(api_token))
+    return (r.status_code, r.text)
 
 
 if __name__ == '__main__':
@@ -48,7 +60,8 @@ if __name__ == '__main__':
     cookies = get_cookies('admin', 'password')
 
     api_token = get_api_token(cookies=cookies)
-    #print(get_datasources(api_token))
-    print(create_datasource_prometheus(api_token=api_token, data_source_name='dcos1', url='http://192.168.84.68:8090'))
+    # print(get_datasources(api_token))
+    # print(create_datasource_prometheus(api_token=api_token, data_source_name='dcos1', url='http://192.168.84.68:8090'))
+    print(delete_datasource_by_name(api_token, 'dcos1'))
     pass
 
