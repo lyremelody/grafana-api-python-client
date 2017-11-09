d10a7d5d7d11cb5b9f167105b006f0bc397c7fea
lyremelody <lyremelody@163.com>
2017-11-09 16:24:26 +0800

    add import dashboard from json file for prometheus datasource

--- gapi_client/dashboards_ex.py
+++ gapi_client/dashboards_ex.py
@@ -37,6 +37,26 @@ def import_gnet_dashboard_for_prometheus_datasource(api_token, gnet_id, promethe
     return (r.status_code, r.text)
 
 
+def import_dashboard_for_prometheus_datasource_from_file(api_token, json_file_name, prometheus_datasource_name):
+    dashboard_json = None
+    input_json = dict()
+    fd = open(json_file_path, 'r')
+    dashboard_json = json.load(fd)
+    input_json = dashboard_json["__inputs"][0]    
+
+    input_json['value'] = prometheus_datasource_name
+
+    payload = { 
+        'dashboard': dashboard_json,
+        'inputs': [input_json],
+        'overwrite': True
+    }   
+
+    uri = 'http://{0}:{1}/api/dashboards/import'.format(GRAFANA_INFO['host'], GRAFANA_INFO['port'])
+    r = requests.post(uri, headers=get_auth_headers(api_token), data=json.dumps(payload))
+    return (r.status_code, r.text)
+
+
 if __name__ == '__main__':
     from common import get_cookies, get_api_token
     cookies = get_cookies('admin', 'password')
